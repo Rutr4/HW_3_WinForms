49e0a8e1e56cbb6fc7a1260d6e2dc963e79b41f2
Artur <a.muradian439@gmail.com>
2023-03-23 13:59:20 +0300

    feat: рефаторинг структур данных, добавил сохранение фигур

--- Form1.cs
+++ Form1.cs
@@ -1,31 +1,36 @@
+using System.Drawing;
 using System.Windows.Forms;
+using static HW_3_WinForms.Form1;
 
 namespace HW_3_WinForms
 {
     public partial class Form1 : Form
     {
         private System.Windows.Forms.Timer moveTimer = new();
+        public enum LineType { None, Curve, Bezier, Polygone, FilledCurve };
+
+        public class Figure // Класс Figure введён для сохранения фигур
+        {
+            public List<Point> points = new();
+
+            public Color pointColor = Color.BlueViolet;
+            public int pointRadius = 8;
+
+            public LineType currentLineType;
+            public Color lineColor = Color.DeepSkyBlue;
+            public int lineWidth = 10;
+        }
+        Figure currentFigure = new();
+        public List<Figure> figuresLst = new(); // Список сохранённых фигур
 
         // Flags
         bool bPoints = true;
-        bool bMove = false;
         bool bDrag = false;
+        bool bMove = false;
+        bool bSave = false;
 
-        #region Properties
-
-        // PointsProps
-        public List<Point> pointsLst = new();
+        // Определение текущей точки
         int draggingPoint;
-        int pointRadius { get; set; } = 8;
-        Color pointColor = Color.Red;
-
-        // LineProps
-        public enum LineType { None, Curve, Bezier, Polygone, FilledCurve };
-        public LineType currentLineType;
-        int lineWidth = 5;
-        Color lineColor = Color.Green;
-
-        #endregion
 
         public Form1()
         {
@@ -42,7 +47,7 @@ namespace HW_3_WinForms
             MouseMove += Form1_MouseMove;
             MouseUp += Form1_MouseUp;
 
-            // buttons
+            // Buttons
             btnPoints.Click += BtnPoints_Click;
             btnMove.Click += BtnMove_Click;
             btnCurve.Click += BtnCurve_click;
@@ -50,6 +55,7 @@ namespace HW_3_WinForms
             btnPolygon.Click += BtnPolygon_click;
             btnFilled.Click += BtnFilled_click;
             btnClear.Click += BtnClear_click;
+            btnSave.Click += BtnSave_Click;
 
             btnSave.Enabled = false;
             btnMove.Enabled = false;
@@ -57,6 +63,10 @@ namespace HW_3_WinForms
             btnCurve.Enabled = false;
             btnFilled.Enabled = false;
             btnPolygon.Enabled = false;
+
+            // Keyboard
+            KeyPreview = true;
+            KeyDown += Form1_keyDown;
             #endregion
 
             #region Timer_Move
@@ -72,35 +82,36 @@ namespace HW_3_WinForms
         }
 
         #region MouseMethods
-        void Form1_MouseClick(object sender, MouseEventArgs e)
+        void Form1_MouseClick(object? sender, MouseEventArgs e)
         {
             if (bPoints && e.Button == MouseButtons.Left)
             {
-                if (pointsLst.Count != 0)
+                if (currentFigure.points.Count != 0)
                 {
                     btnPolygon.Enabled = true;
-                    if (pointsLst.Count >= 2)
+                    if (currentFigure.points.Count >= 2)
                     {
+                        btnSave.Enabled = true;
                         btnFilled.Enabled = true;
                         btnCurve.Enabled = true;
-                        if (pointsLst.Count >= 3)
+                        if (currentFigure.points.Count >= 3)
                             btnBezier.Enabled = true;
                     }
                 }
                 // Сохраняем координаты курсора мышки
-                int pointHalfRadius = pointRadius / 2;
+                int pointHalfRadius = currentFigure.pointRadius / 2;
                 Point p = new(e.X - pointHalfRadius, e.Y - pointHalfRadius);
                 // Добавляем в коллекцию точек
-                pointsLst.Add(p);
+                currentFigure.points.Add(p);
                 // Генерируем событие Paint для перерисовки точек
                 Refresh();
             }
         }
-        void Form1_MouseDown(object sender, MouseEventArgs e)
+        void Form1_MouseDown(object? sender, MouseEventArgs e)
         {
-            for (int i = 0; i < pointsLst.Count; i++)
+            for (int i = 0; i < currentFigure.points.Count; i++)
             {
-                if (IsTargetPoint(pointsLst[i], e.Location))
+                if (IsTargetPoint(currentFigure.points[i], e.Location))
                 {
                     bPoints = !bPoints;
                     bDrag = true;
@@ -110,15 +121,16 @@ namespace HW_3_WinForms
                 }
             }
         }
-        void Form1_MouseMove(object sender, MouseEventArgs e)
+        void Form1_MouseMove(object? sender, MouseEventArgs e)
         {
             if (bDrag)
             {
-                pointsLst[draggingPoint] = new Point(e.Location.X - pointRadius / 2, e.Location.Y - pointRadius / 2);
+                int pointHalfRadius = currentFigure.pointRadius / 2;
+                currentFigure.points[draggingPoint] = new Point(e.Location.X - pointHalfRadius, e.Location.Y - pointHalfRadius);
                 Refresh();
             }
         }
-        void Form1_MouseUp(object sender, MouseEventArgs e)
+        void Form1_MouseUp(object? sender, MouseEventArgs e)
         {
             if (bDrag)
             {
@@ -131,42 +143,66 @@ namespace HW_3_WinForms
         void Form1_Paint(object? sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            if (pointsLst.Count != 0)
+            if (figuresLst.Count != 0)
             {
-                ShowPoints(g);
-                if (currentLineType != LineType.None)
+                foreach (var fig in figuresLst)
                 {
-                    ShowLine(g, currentLineType);
+                    ShowPoints(g, fig);
+                    if (fig.currentLineType != LineType.None)
+                    {
+                        ShowLines(g, fig.currentLineType);
+                    }
+                }
+            }
+            if (currentFigure.points.Count != 0)
+            {
+                ShowPoints(g, currentFigure);
+                if (currentFigure.currentLineType != LineType.None)
+                {
+                    ShowLines(g, currentFigure.currentLineType);
                 }
             }
         }
-        void ShowPoints(Graphics g)
+        void ShowPoints(Graphics g, Figure fig)
         {
-            foreach (var p in pointsLst)
+            foreach (var p in fig.points)
             {
-                g.FillEllipse(new SolidBrush(pointColor), p.X, p.Y, pointRadius, pointRadius);
+                g.FillEllipse(new SolidBrush(fig.pointColor), p.X, p.Y, fig.pointRadius, fig.pointRadius);
             }
         }
-        void ShowLine(Graphics g, LineType currentLineType)
-        {
-            SolidBrush brush = new SolidBrush(lineColor);
-            Pen pen = new Pen(brush, lineWidth);
-
-            Point[] pointsArray = new Point[pointsLst.Count];
-            int pointHalfRadius = pointRadius / 2;
-
-            for (int i = 0; i < pointsLst.Count; i++)
+        void ShowLines(Graphics g, LineType currentLineType)
+        { 
+            if (figuresLst.Count != 0)
             {
-                pointsArray[i].X = pointsLst[i].X + pointHalfRadius;
-                pointsArray[i].Y = pointsLst[i].Y + pointHalfRadius;
-            } 
+                foreach (var figure in figuresLst)
+                {
+                    ShowLineOfFigure(figure, g);
+                }
+            }
+            if (currentFigure.points.Count != 0)
+            {
+                ShowLineOfFigure(currentFigure, g);
+            }
+        }
+        void ShowLineOfFigure(Figure fig, Graphics g)
+        {
+            SolidBrush brush = new(fig.pointColor);
+            Pen pen = new(brush, fig.lineWidth);
+            Point[] pointsArray = new Point[fig.points.Count];
+            int pointHalfRadius = fig.pointRadius / 2;
 
-            switch (currentLineType)
+            for (int i = 0; i < fig.points.Count; i++)
+            {
+                pointsArray[i].X = fig.points[i].X + pointHalfRadius;
+                pointsArray[i].Y = fig.points[i].Y + pointHalfRadius;
+            }
+
+            switch (fig.currentLineType)
             {
                 // https://learn.microsoft.com/ru-ru/dotnet/api/system.drawing.graphics.drawbeziers?view=dotnet-plat-ext-7.0
-                // Количество точек в массиве должно быть кратно трем плюс 1, например 4, 7 или 10.
+                // Для Безье количество точек в массиве должно быть кратно трем плюс 1, например 4, 7 или 10.
                 case LineType.Bezier:
-                    if (pointsLst.Count > 3 && (pointsLst.Count - 1) % 3 == 0)
+                    if (fig.points.Count > 3 && (fig.points.Count - 1) % 3 == 0)
                         g.DrawBeziers(pen, pointsArray);
                     break;
                 case LineType.Curve:
@@ -176,13 +212,16 @@ namespace HW_3_WinForms
                     g.DrawPolygon(pen, pointsArray);
                     break;
                 case LineType.FilledCurve:
+                    g.DrawClosedCurve(pen, pointsArray);
                     g.FillClosedCurve(brush, pointsArray);
                     break;
                 default:
                     break;
             }
         }
-        void BtnPoints_Click(object sender, EventArgs e)
+
+
+        void BtnPoints_Click(object? sender, EventArgs e)
         {
             bPoints = !bPoints;
             bDrag = false;
@@ -193,14 +232,19 @@ namespace HW_3_WinForms
         {
             
         } //TODO: РЕАЛИЗОВАТЬ
+        void BtnSave_Click(object? sender, EventArgs e)
+        {
+            figuresLst.Add(currentFigure);
+            currentFigure = new();
+        }
         
         // Находится ли курсор над одной из точек в момент нажатия
         bool IsTargetPoint(Point pixelPoint, Point cursor)
         {
             if (cursor.X >= pixelPoint.X &&
-                cursor.X <= pixelPoint.X + pointRadius &&
+                cursor.X <= pixelPoint.X + currentFigure.pointRadius &&
                 cursor.Y >= pixelPoint.Y &&
-                cursor.Y <= pixelPoint.Y + pointRadius)
+                cursor.Y <= pixelPoint.Y + currentFigure.pointRadius)
                 return true;
             else
                 return false;
@@ -209,34 +253,34 @@ namespace HW_3_WinForms
         // Отрисовка фигуры по заданным точкам
         void BtnCurve_click(object? sender, EventArgs e)
         {
-            if (pointsLst.Count != 0)
+            if (currentFigure.points.Count != 0)
             {
-                if (currentLineType != LineType.Curve)
-                    currentLineType = LineType.Curve;
+                if (currentFigure.currentLineType != LineType.Curve)
+                    currentFigure.currentLineType = LineType.Curve;
                 else
-                    currentLineType = LineType.None;
+                    currentFigure.currentLineType = LineType.None;
                 Refresh();
             }
         }
         void BtnBezier_click(object? sender, EventArgs e)
         {
-            if (pointsLst.Count != 0)
+            if (currentFigure.points.Count != 0)
             {
-                if (currentLineType != LineType.Bezier)
-                    currentLineType = LineType.Bezier;
+                if (currentFigure.currentLineType != LineType.Bezier)
+                    currentFigure.currentLineType = LineType.Bezier;
                 else
-                    currentLineType = LineType.None;
+                    currentFigure.currentLineType = LineType.None;
                 Refresh();
             }
         }
         void BtnPolygon_click(object? sender, EventArgs e)
         {
-            if (pointsLst.Count != 0)
+            if (currentFigure.points.Count != 0)
             {
-                if (currentLineType != LineType.Polygone)
-                    currentLineType = LineType.Polygone;
+                if (currentFigure.currentLineType != LineType.Polygone)
+                    currentFigure.currentLineType = LineType.Polygone;
                 else
-                    currentLineType = LineType.None;
+                    currentFigure.currentLineType = LineType.None;
                 Refresh();
             }
         }
@@ -244,12 +288,12 @@ namespace HW_3_WinForms
         // Закрашивание фигуры
         void BtnFilled_click(object? sender, EventArgs e)
         {
-            if (pointsLst.Count != 0)
+            if (currentFigure.points.Count != 0)
             {
-                if (currentLineType != LineType.FilledCurve)
-                    currentLineType = LineType.FilledCurve;
+                if (currentFigure.currentLineType != LineType.FilledCurve)
+                    currentFigure.currentLineType = LineType.FilledCurve;
                 else
-                    currentLineType = LineType.None;
+                    currentFigure.currentLineType = LineType.None;
                 Refresh();
             }
         }
@@ -257,16 +301,62 @@ namespace HW_3_WinForms
         // Очистка формы
         void BtnClear_click(object? sender, EventArgs e)
         {
-            if (pointsLst.Count != 0)
+            if (currentFigure.points.Count != 0)
             {
-                pointsLst.Clear();
+                currentFigure.points.Clear();
                 btnBezier.Enabled = false;
                 btnCurve.Enabled = false;
                 btnFilled.Enabled = false;
                 btnPolygon.Enabled = false;
-                currentLineType = LineType.None;
+                currentFigure.currentLineType = LineType.None;
+            }
+            if (figuresLst.Count != 0)
+            {
+                figuresLst.Clear();
             }
             Refresh();
         }
+
+        // Обработка нажатия клавиш клавиатуры
+        void Form1_keyDown(object? sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape: // Очистка формы
+                    btnClear.PerformClick();
+                    break;
+                case Keys.Space: // Включает/выключает режим движения
+                    break;
+                case Keys.Oemplus: // Увеличивает  скорость движения точек
+                    break;
+                case Keys.OemMinus: // Уменьшает скорость движения точек
+                    break;
+                default:
+                    break;
+            }
+        } // TODO: ДОДЕЛАТЬ (SPACE, +, -)
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            bool IsHandled = true;
+            switch (keyData)
+            {
+                case Keys.Up:
+                    
+                    break;
+                case Keys.Down:
+                    
+                    break;
+                case Keys.Left:
+                    
+                    break;
+                case Keys.Right:
+                    
+                    break;
+                default:
+                    IsHandled = false;
+                    break;
+            }
+            return IsHandled;
+        } // TODO: ДОДЕЛАТЬ
     }
 }
